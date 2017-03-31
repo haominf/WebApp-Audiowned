f5c86e6b947abe9bfe2629ba055463ef2c078c83
Michelle Luo <luo.mich16@gmail.com>
2017-03-30 22:15:07 -0400

    Include css directory

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require(fs);
 var app = express();
 
 var port = process.env.PORT || 8080;
@@ -7,7 +8,7 @@ var port = process.env.PORT || 8080;
 app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/css'));
 
 // set the home page route
 app.get('/', (req, res) => {
