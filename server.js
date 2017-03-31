c3c9bc5d3a06beb584b07d209d993ae46805f04e
Michelle Luo <luo.mich16@gmail.com>
2017-03-30 20:47:37 -0400

    Initialize heroku files

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.log('App running on localhost:' + port);
+});
