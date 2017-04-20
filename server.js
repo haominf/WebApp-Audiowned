c2d358ca2b0624f8d869e340ed94f09138ecdf3b
HAOMIN FENG <haomin.feng@tufts.edu>
2017-04-20 02:45:47 -0400

    debugging

--- server.js
+++ server.js
@@ -92,8 +92,7 @@ app.get('/loading', function(req, res) {
 
 app.get('/matched', function(req, res) {
   console.log('enter matched');
-  res.redirect('/callback');
-  // res.render('matched', {Name:player_name, Pic_URL:player_pic});
+  res.render('index');
 });
 
 app.get('/callback', function(req, res) {
