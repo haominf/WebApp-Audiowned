19c117d639a1741caa70e65d5bac2b959e4009dd
Michelle Luo <luo.mich16@gmail.com>
2017-04-27 17:52:09 -0400

    Fix tabbing & set localhost callback

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+// express/node things
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var querystring = require('querystring');
@@ -9,11 +9,30 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var spotify_api = "https://api.spotify.com/";
+// set port number
+app.set('port', (process.env.PORT || 8888));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+// make express look in the views/pages directory for assets (css/js/img)
+app.set('views', __dirname + '/views/pages');
+// virtual path prefix - "puts" css/js/img in this dir
+app.use('/static', express.static('public')).use(cookieParser());
 
+// mongo things
+var mongoUri = process.env.MONGODB_URI || process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL || 'mongodb://localhost/';
+var MongoClient = require('mongodb').MongoClient;
+var format = require('util').format;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+    db = databaseConnection;
+});
+
+// spotify things
+var spotify_api = "https://api.spotify.com/";
 var client_id = '67fd18a6482b41a5aa0c8b71b1517989'; // Your client id
 var client_secret = '7a42b826ed224ed0a94634b2d12152b6'; // Your secret
-var redirect_uri = 'https://audiowned.herokuapp.com/callback'; // local: 'http://localhost:8888/callback'
+var redirect_uri = 'https://audiowned.herokuapp.com/callback' || 'http://localhost:' + app.get('port') + '/callback';
+
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
@@ -30,24 +49,6 @@ var generateRandomString = function(length) {
 };
 
 var stateKey = 'spotify_auth_state';
-
-// set port number
-app.set('port', (process.env.PORT || 8888));
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-// make express look in the views/pages directory for assets (css/js/img)
-app.set('views', __dirname + '/views/pages');
-// virtual path prefix - "puts" css/js/img in this dir
-app.use('/static', express.static('public')).use(cookieParser());
-
-// set up mongo for future use
-var mongoUri = process.env.MONGODB_URI || process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL || 'mongodb://localhost/';
-var MongoClient = require('mongodb').MongoClient;
-var format = require('util').format;
-var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
-    db = databaseConnection;
-});
 
 // set the home page route
 app.get('/', function(req, res) {
@@ -78,24 +79,21 @@ app.post('/login', function(req, res) {
 
 app.get('/home', function(req, res) {
     res.render('home&loading', {Name:player_name, Pic_URL:player_pic});
-
 });
 
 app.get('/login', function(req, res) {
-
-  var state = generateRandomString(16);
-  res.cookie(stateKey, state);
-
-  // your application requests authorization
-  var scope = 'user-read-private user-read-email';
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
+      var state = generateRandomString(16);
+      res.cookie(stateKey, state);
+      // app requests authorization
+      var scope = 'user-read-private user-read-email';
+      res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+          response_type: 'code',
+          client_id: client_id,
+          scope: scope,
+          redirect_uri: redirect_uri,
+          state: state
+        }));
 });
 
 app.get('/loading', function(req, res) {
@@ -127,75 +125,70 @@ app.get('/game', function(req, res) {
 
 
 app.post('/submit', function(req, res) {
-        console.log(req.body);
-        console.log('in game');
-        res.render('game', {Name:player_name, Pic_URL:player_pic});
+    console.log(req.body);
+    console.log('in game');
+    res.render('game', {Name:player_name, Pic_URL:player_pic});
 });
 
 app.post('/submit', function(req, res) {
-        console.log(req.body);
->>>>>>> cb645eed168651c837d28d8ee78c081db5cfae79
-	console.log("hi");
+    console.log(req.body);
 });
 
 app.get('/callback', function(req, res) {
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
 
-  // your application requests refresh and access tokens
-  // after checking the state parameter
-
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
-
-  if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
-  } else {
-    res.clearCookie(stateKey);
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
-
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
-
-        var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
-
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log('this is the json body:');
-          console.log(body);
-          player_json = body;
-          player_name = player_json['display_name'];
-          player_pic = player_json['images'][0]['url'];
-          res.render('home&loading', {Name:player_name, Pic_URL:player_pic});
-        });
-      } else {
+    if (state === null || state !== storedState) {
         res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
-      }
-    });
-  }
+            querystring.stringify({
+                error: 'state_mismatch'
+        }));
+    } 
+    else {
+        res.clearCookie(stateKey);
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+            },
+            json: true
+        };
+        // send request to spotify 
+        request.post(authOptions, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var access_token = body.access_token,
+                    refresh_token = body.refresh_token;
+                var options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+                // use the access token to access the Spotify Web API
+                request.get(options, function(error, response, body) {
+                    console.log('this is the json body:');
+                    console.log(body);
+                    player_json = body;
+                    player_name = player_json['display_name'];
+                    player_pic = player_json['images'][0]['url'];
+                    res.render('home&loading', {Name:player_name, Pic_URL:player_pic});
+                });
+            } 
+            else {
+                res.redirect('/#' +
+                querystring.stringify({
+                    error: 'invalid_token'
+                }));
+            }
+        });
+    }
 });
 
 app.listen(app.get('port'), function() {
